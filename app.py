a231ea1b741708472ba5429bf17d90bb9295b011
Dias Zakir <diaszakir1@outlook.com>
2025-09-06 23:51:47 +0500

    Book model and BookRequest model for data validation

--- app.py
+++ app.py
@@ -5,5 +5,33 @@ from starlette import status
 
 app = FastAPI()
 
+class Book:
+    def __init__(self, id, title, author, published_date, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.published_date = published_date
+        self.description = description
+        self.rating = rating
+
+
+class BookRequest(BaseModel):
+    id: Optional[int] = Field(description="ID is not needed", default=None) # we will use auto-increment
+    title: str = Field(min_length=4)
+    author: str = Field(min_length=4)
+    published_date: int = Field(gt=1899, lt=2100)
+    description: str = Field(max_length=100)
+    rating: str = Field(gt=0, lt=6)
+
+    model_config = {
+        "json_scheme_extra": {
+            "title": "Atomic Habits",
+            "author": "James Clear",
+            "published_date": 2018,
+            "description": "A book about how to implement good habits",
+            "rating": 5
+        }
+    }
+
 # Test data
 books = []
